8a5834d152e383f48e66e043f007594abe28e777
Covariancee <okaycan.yurt@gmail.com>
2023-07-21 12:32:35 +0300

    profile-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:sertan/provider/category_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:sertan/pages/category_page.dart';
 import 'package:sertan/pages/login_page.dart';
-import 'package:provider/provider.dart';
 import 'package:sertan/pages/profile_page.dart';
 import 'package:sertan/pages/register_page.dart';
+import 'package:sertan/pages/terms_page.dart';
 import 'package:sertan/provider/city_and_district_provider.dart';
 
 void main() {
@@ -31,7 +31,9 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => RegisterPageView(),
         '/login-page': (context) => LoginPageView(),
-        '/profile-page': (context) => ProfilePage()
+        '/profile-page': (context) => ProfilePage(),
+        '/terms-page': (context) => const TermsPage(),
+        '/category-page': (context) => CategoryPageView(),
       },
     );
   }
